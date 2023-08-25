70c35e0f5159d25264d184b9b9f508cb2bc22799
sharvils <sharvil.shah@intel.com>
2023-08-24 20:18:51 -0700

    debug level logging added for horovod test

--- preset/deep-learning/tests/horovod/run.sh
+++ preset/deep-learning/tests/horovod/run.sh
@@ -3,4 +3,4 @@
 source activate tensorflow
 
 SCRIPT_DIR="$(dirname $0)"
-horovodrun --verbose -np 2 -H localhost:2 -p 12345 python $SCRIPT_DIR/tensorflow2_keras_mnist.py
+horovodrun --verbose -np 2 --log-level=DEBUG -H localhost:2 -p 12345 python $SCRIPT_DIR/tensorflow2_keras_mnist.py
