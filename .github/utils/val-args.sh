3946047ee5f57d99dae17832dd32b6b5a8997817
Tyler Titsworth <tyler.titsworth@intel.com>
2024-02-08 15:36:22 -0800

    Builder Hotfix (#191)
    * add certs
    
    * fix run number typo
    
    * remove null terminating regex
    
    * add default

--- .github/utils/val-args.sh
+++ .github/utils/val-args.sh
@@ -16,7 +16,7 @@ fi
 
 # Use regex to extract key-value pairs with the cases:
 # The regex now ensures that there is at least one 'a-zA-Z' value present
-regex="([^\x00a-zA-Z]+[^\x00a-zA-Z0-9_-]+)\s*[:=]\s*([^\x00a-zA-Z0-9\/=_\-\s']+)"
+regex="([a-zA-Z]+[a-zA-Z0-9_-]+)\s*[:=]\s*([a-zA-Z0-9\/=_\-\s']+)"
 key_val_pairs=$(echo "$matched_line" | grep -oP "$regex" | sed 's/: /=/g')
 
 # Check if there is at least one key-value pair
