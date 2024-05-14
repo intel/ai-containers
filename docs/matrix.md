ed5d90584f53df96de525b88b9b454ee5dc674a7
Tyler Titsworth <tyler.titsworth@intel.com>
2024-05-14 13:58:33 -0700

    Documentation Automation (#273)
    * init rtd
    
    * update config
    
    * switch to mkdocs
    
    * pre-commit
    
    * test
    
    * add metadata
    
    * add table generation for python and pytorch
    
    * rerun pre-commit
    
    * add comments and exclusionary functionality
    
    * utilize new python base svc
    
    * return isort cfg
    
    * fix pre-commit isort config
    
    * add horizontal frame building
    
    * add get_repo method
    
    * add docs ci
    
    * swap to internal runners
    
    * remove folder filtering on pr
    
    * add container labelling scheme
    
    * add concurrency
    
    * add job read clause
    
    * add reqs install step
    
    * return dockerhub docs
    
    * update mkdocs config
    
    * add readmes to site
    
    * refactor scripts
    
    * re-enable mkdocs hook
    
    * add improved python dep tracking
    
    * correct reqs
    
    * update xpu reqs
    
    * refac 2
    
    * return proxies
    
    * remove import regression
    
    * return depends_on to remove regression
    
    * Update Dockerfile
    
    * Update docker-compose.yaml
    
    * Update Dockerfile

---
+++ docs/matrix.md
@@ -0,0 +1,41 @@
+# Support Matrix
+
+=== "Framework Containers"
+    === "Python"
+        {{ read_csv('assets/python.csv') }}
+    === "Classical ML"
+        {{ read_csv('assets/classical-ml.csv') }}
+    === "PyTorch"
+        {{ read_csv('assets/pytorch.csv') }}
+    === "TensorFlow"
+        {{ read_csv('assets/tensorflow.csv') }}
+
+=== "Model Containers"
+    === "CPU"
+        === "PyTorch"
+            {{ read_csv('assets/pytorch-cpu.csv') }}
+        === "TensorFlow"
+            {{ read_csv('assets/tensorflow-cpu.csv') }}
+    === "PVC"
+        === "Flex"
+            {{ read_csv('assets/flex-pvc.csv') }}
+        === "Max"
+            {{ read_csv('assets/max-pvc.csv') }}
+
+=== "Preset Containers"
+    === "Data Analytics"
+        {{ read_csv('assets/data_analytics.csv') }}
+    === "Classical ML"
+        {{ read_csv('assets/classical_ml.csv') }}
+    === "Deep Learning"
+        {{ read_csv('assets/deep_learning.csv') }}
+    === "Inference Optimization"
+        {{ read_csv('assets/inference_optimization.csv') }}
+
+=== "Other"
+    === "Serving"
+        {{ read_csv('assets/serving.csv') }}
+    === "Transformers"
+        {{ read_csv('assets/transformers.csv') }}
+    === "GenAI"
+        {{ read_csv('assets/genai.csv') }}
---
+++ docs/scripts/readmes.py
@@ -0,0 +1,56 @@
+# Copyright (c) 2024 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+
+readmes = [
+    "classical-ml/README.md",
+    "preset/README.md",
+    "python/README.md",
+    "pytorch/README.md",
+    "pytorch/serving/README.md",
+    "tensorflow/README.md",
+]
+
+
+def copy_readmes():
+    """
+    Copies README.md files to the docs directory.
+
+    Returns:
+        None
+    """
+    shutil.copy("README.md", "docs/index.md")
+    for readme in readmes:
+        os.makedirs(os.path.dirname(f"docs/{readme}"), exist_ok=True)
+        shutil.copy(readme, f"docs/{readme}")
+
+
+def remove_readmes():
+    """
+    Removes README.md files added to docs directory by copy_readmes()
+
+    Returns:
+        None
+    """
+    try:
+        os.remove("docs/index.md")
+    except OSError:
+        pass
+    for readme in readmes:
+        try:
+            shutil.rmtree(os.path.dirname(f"docs/{readme}"))
+        except:
+            pass
