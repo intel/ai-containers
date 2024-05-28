474f4874532c63ae7af42da7654952c544703c5f
tylertitsworth <tyler.titsworth@intel.com>
2024-05-28 14:01:33 -0700

    skip tf tests lint

--- tensorflow/tests/tf_base_test.py
+++ tensorflow/tests/tf_base_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# pylint: skip-file
 import intel_extension_for_tensorflow as itex
 import numpy as np
 import tensorflow as tf
