d7191337f123307cc5e5274a57bfec8f3f8c4625
dmsuehir <dina.s.jones@intel.com>
2024-06-05 17:18:15 -0700

    Formatting
    Signed-off-by: dmsuehir <dina.s.jones@intel.com>

--- workflows/charts/training/huggingface_llm/generate_ssh_keys.sh
+++ workflows/charts/training/huggingface_llm/generate_ssh_keys.sh
@@ -16,15 +16,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-function gen_single_key()
-{
+function gen_single_key() {
     ALG_NAME=$1
-    if [[ ! -f /etc/ssh/ssh_host_${ALG_NAME}_key ]]
-    then
+    if [[ ! -f /etc/ssh/ssh_host_${ALG_NAME}_key ]]; then
         ssh-keygen -q -N "" -t ${ALG_NAME} -f /etc/ssh/ssh_host_${ALG_NAME}_key
     fi
 }
-
 
 gen_single_key dsa
 gen_single_key rsa
