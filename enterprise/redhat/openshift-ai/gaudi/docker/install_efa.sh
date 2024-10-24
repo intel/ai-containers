b6216ea004947b9af964db66325ea86fd8af62a0
Sharvil Shah <sharvil.shah@intel.com>
2024-10-24 13:11:29 -0700

    Gaudi 1.18 openshift notebook container (#480)
    Signed-off-by: sharvil10 <sharvil.shah@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Tyler Titsworth <tyler.titsworth@intel.com>

--- enterprise/redhat/openshift-ai/gaudi/docker/install_efa.sh
+++ enterprise/redhat/openshift-ai/gaudi/docker/install_efa.sh
@@ -14,27 +14,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEFAULT_EFA_INSTALLER_VER=1.29.0
+#!/bin/bash -ex
+
+DEFAULT_EFA_INSTALLER_VER=1.34.0
 efa_installer_version=${1:-$DEFAULT_EFA_INSTALLER_VER}
 
 tmp_dir=$(mktemp -d)
 wget -nv https://efa-installer.amazonaws.com/aws-efa-installer-"$efa_installer_version".tar.gz -P "$tmp_dir"
 tar -xf "$tmp_dir"/aws-efa-installer-"$efa_installer_version".tar.gz -C "$tmp_dir"
+RUN_EFA_INSTALLER="./efa_installer.sh -y --skip-kmod --skip-limit-conf --no-verify"
 pushd "$tmp_dir"/aws-efa-installer
 # shellcheck disable=SC1091
-case $(
-	. /etc/os-release
-	echo -n "$ID"
-) in
+. /etc/os-release
+case $ID in
 rhel)
 	# we cannot install dkms packages on RHEL images due to OCP rules
-	rm -f RPMS/RHEL8/x86_64/dkms*.rpm
+	find RPMS/ -name 'dkms*.rpm' -exec rm -f {} \;
+	find RPMS/ -name 'efa-*.rpm' -exec rm -f {} \;
+	case $VERSION_ID in
+	8*)
+		dnf install -y RPMS/ROCKYLINUX8/x86_64/rdma-core/*.rpm
+		;;
+	9*)
+		dnf install -y RPMS/ROCKYLINUX9/x86_64/rdma-core/*.rpm
+		;;
+	*)
+		echo "Unsupported RHEL version: $VERSION_ID"
+		exit 1
+		;;
+	esac
+	RUN_EFA_INSTALLER="echo 'Skipping EFA installer on RHEL'"
 	;;
 tencentos)
-	dnf install -y RPMS/ROCKYLINUX8/x86_64/rdma-core/libibverbs-46.0-1.el8.x86_64.rpm RPMS/ROCKYLINUX8/x86_64/rdma-core/libibverbs-utils-46.0-1.el8.x86_64.rpm
+	dnf install -y RPMS/ROCKYLINUX8/x86_64/rdma-core/*.rpm
 	patch -f -p1 -i /tmp/tencentos_efa_patch.txt --reject-file=tencentos_efa_patch.rej --no-backup-if-mismatch
 	;;
 esac
-./efa_installer.sh -y --skip-kmod --skip-limit-conf --no-verify
+eval "$RUN_EFA_INSTALLER"
 popd
 rm -rf "$tmp_dir"
