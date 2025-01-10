eb9c8ef2a983b44c87554c688bf3fce560702623
sharvil10 <sharvil.shah@intel.com>
2025-01-10 12:59:37 -0500

    shell format fixed
    Signed-off-by: sharvil10 <sharvil.shah@intel.com>

--- enterprise/redhat/openshift-ai/gaudi/docker/install-python310.sh
+++ enterprise/redhat/openshift-ai/gaudi/docker/install-python310.sh
@@ -20,32 +20,32 @@ _SSL_LIB=""
 
 # preinstall dependencies and define variables
 case "${_BASE_NAME}" in
-    *ubuntu22.04*)
-        echo "Skip install Python3.10 from source on Ubuntu22.04"
-        exit 0;
-    ;;
-    *ubuntu*)
-        apt update
-        apt install -y libsqlite3-dev libreadline-dev
-    ;;
-    *rhel*)
-        dnf install -y sqlite-devel readline-devel xz-devel
-    ;;
-    *tencentos3.1*)
-        dnf install -y sqlite-devel readline-devel zlib-devel xz-devel bzip2-devel libffi-devel
-        wget -nv -O /opt/openssl-1.1.1w.tar.gz https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1w/openssl-1.1.1w.tar.gz && \
-            cd /opt/ && \
-            tar xzf openssl-1.1.1w.tar.gz && \
-            rm -rf openssl-1.1.1w.tar.gz && \
-            cd openssl-1.1.1w && \
-            ./config --prefix=/usr/local/openssl-1.1.1w shared zlib && \
-            make && make install
-        ln -s /etc/pki/tls/cert.pem /usr/local/openssl-1.1.1w/ssl/cert.pem
+*ubuntu22.04*)
+	echo "Skip install Python3.10 from source on Ubuntu22.04"
+	exit 0
+	;;
+*ubuntu*)
+	apt update
+	apt install -y libsqlite3-dev libreadline-dev
+	;;
+*rhel*)
+	dnf install -y sqlite-devel readline-devel xz-devel
+	;;
+*tencentos3.1*)
+	dnf install -y sqlite-devel readline-devel zlib-devel xz-devel bzip2-devel libffi-devel
+	wget -nv -O /opt/openssl-1.1.1w.tar.gz https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1w/openssl-1.1.1w.tar.gz &&
+		cd /opt/ &&
+		tar xzf openssl-1.1.1w.tar.gz &&
+		rm -rf openssl-1.1.1w.tar.gz &&
+		cd openssl-1.1.1w &&
+		./config --prefix=/usr/local/openssl-1.1.1w shared zlib &&
+		make && make install
+	ln -s /etc/pki/tls/cert.pem /usr/local/openssl-1.1.1w/ssl/cert.pem
 
-        PATH=$PATH:/usr/local/protoc/bin:/usr/local/openssl-1.1.1w/bin
-        LD_LIBRARY_PATH=/usr/local/openssl-1.1.1w/lib:$LD_LIBRARY_PATH
-        _SSL_LIB="--with-openssl=/usr/local/openssl-1.1.1w"
-    ;;
+	PATH=$PATH:/usr/local/protoc/bin:/usr/local/openssl-1.1.1w/bin
+	LD_LIBRARY_PATH=/usr/local/openssl-1.1.1w/lib:$LD_LIBRARY_PATH
+	_SSL_LIB="--with-openssl=/usr/local/openssl-1.1.1w"
+	;;
 esac
 
 # install Python
@@ -59,17 +59,17 @@ make -j && make altinstall
 
 # post install
 case "${_BASE_NAME}" in
-    *rhel9*)
-        export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-    ;;
-    *tencentos3.1*)
-        alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.10 4 && \
-        alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 3 && \
-        alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2 && \
-        alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1 && \
-        alternatives --set python3 /usr/local/bin/python3.10
-        export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
-    ;;
+*rhel9*)
+	export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+	;;
+*tencentos3.1*)
+	alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.10 4 &&
+		alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 3 &&
+		alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 2 &&
+		alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1 &&
+		alternatives --set python3 /usr/local/bin/python3.10
+	export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+	;;
 esac
 
 python3 -m pip install --upgrade pip setuptools
