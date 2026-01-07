d611cb085de52b6c6b09163a3456a44df0a13ae6
Patil, Jitendra <jitendra.patil@intel.com>
2026-01-07 08:27:30 -0800

    removed images that are no longer maintained, updated docs accordingly
    Signed-off-by: Patil, Jitendra <jitendra.patil@intel.com>

--- docs/scripts/hook.py
+++ docs/scripts/hook.py
@@ -33,14 +33,9 @@ def create_support_matrix():
     compose_to_csv("pytorch", None)
     compose_to_csv("pytorch", "serving")
     compose_to_csv("tensorflow", None)
-    compose_to_csv("classical-ml", None)
     compose_to_csv("jax", None)
 
     # get_repo(models)
-    compose_to_csv("preset/classical-ml", "classical_ml")
-    compose_to_csv("preset/deep-learning-pytorch-cpu", "deep_learning_pytorch_cpu")
-    compose_to_csv("preset/deep-learning-pytorch-gpu", "deep_learning_pytorch_gpu")
-    compose_to_csv("preset/deep-learning-jax-cpu", "deep_learning_jax_cpu")
 
 
 def on_pre_build(*args, **kwargs):
